7b811f951bc5d9955a2523b113ed4b471d69705a
S Mahesh <98635199+smahesh77@users.noreply.github.com>
2023-05-27 18:09:01 +0530

    Update postRoute.js

--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -14,7 +14,7 @@ router.get('/ById/:id', async (req, res) => {
 
 })
 
-router.post('/', async (req, res) => {
+router.post('/',validateToken, async (req, res) => {
     const post = req.body;
     console.log(post)
     const newPost = new postModel(post)
